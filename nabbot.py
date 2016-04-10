a3f4b5fdc605345fae23f8dca27ee2bf86318475
Allan Galarza <allan.galarza@gmail.com>
2016-04-10 08:38:44 -0700

    Initial commit

---
+++ nabbot.py
@@ -0,0 +1,347 @@
+import discord
+import math
+from discord.ext import commands
+import random
+import asyncio
+import urllib.request
+import urllib
+import re
+import time
+import datetime
+from datetime import timedelta
+import sqlite3
+from login import username, password
+
+description = '''Mission: Destroy all humans.'''
+bot = commands.Bot(command_prefix='/', description=description)
+
+client = discord.Client()
+
+########some global variables to give u cancer
+#lastmessage stuff
+lastmessage = None
+lastmessagetime = datetime.datetime.now()
+#a boolean to know if a goofing msg was the last thing we saw
+isgoof = False
+idlemessages = ["Galarzazzzzza is a nab, i know, i know, oh oh oh",
+"Did you know 9 out of 10 giant spiders prefer nabchow?",
+"Any allegations made about Nezune and corpses are nothing but slander!",
+"All hail Michu, our cat overlord.",
+"Beware of nomads, they are known to kill unsuspecting druids!"]
+#main channel where the bot chats for luls
+#this is so we can keep track of idletime for this server only
+#and do timed shit in here
+mainserver = "Nab Bot"
+mainchannel = "general"
+mainchannel_idletime = timedelta(seconds=0)
+
+userlist = []
+########
+
+@bot.event
+@asyncio.coroutine
+def on_ready():
+    global idlemessages
+    bot.load_extension("tibia")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    #load the userList
+    userList("load")
+    #call think() when everything's ready
+    #galarzaaisanab = '<@{0.id}>'.format(getUserByName("Galarzaa"))
+    #idlemessages = [""+galarzaaisanab+" is a nab, i know, i know, oh oh oh"]
+    yield from think()
+
+
+########a think function!
+@asyncio.coroutine
+def think():
+    #i could do something like, check if the bots alive instead of just a "while true" but i dont see the point.
+    while 1:
+        #update idle time
+        updateChannelIdleTime()
+        
+        #example function goof() will say some random shit after 30 secs of idle time
+        yield from goof()
+        
+        #do any magic we want here
+        #(this is a good spot to have a function that periodically crawls online lists and checks for levelups etc)
+        
+        
+        #sleep for a bit and then loop back
+        yield from asyncio.sleep(5)
+########
+
+########update idle time, dunno if u wanna use a function for this, just seemed less ugly
+def updateChannelIdleTime():
+    global mainchannel_idletime
+    mainchannel_idletime = datetime.datetime.now() - lastmessagetime
+########
+
+@asyncio.coroutine
+def goof():
+    global mainserver
+    global mainchannel
+    global mainchannel_idletime
+    global lastmessage
+    global isgoof
+    #after 30 secs of silence the bot will say some random shit
+    #it wont say anything if it hasnt seen any msgs since it goof'd
+    if lastmessage != None and isgoof == False and mainchannel_idletime > timedelta(seconds=30):# and (not lastmessage.author.id == bot.user.id): #<< dont need this anymore
+        channel = getChannelByServerAndName(mainserver,mainchannel)
+        yield from bot.send_message(channel,random.choice(idlemessages))
+        #this im kinda worried about, it seems to work but i'm not sure if it CANT fuck up
+        #afaik, it shouldnt always be seeing its own msg instantly and if yield from means "dont wait for this to be done" then sometimes the isgoof should be set to true and then immediatly set back to false?
+        #worked in all my tests though, so we'll see. worse case scenario it sometimes doesnt realize and goofs twice in a row
+        isgoof = True
+
+########i cant use bot.say in think() because theres no context here for the bot to know what channel its supposed to respond to!
+########so i made these two (now three!) funcs (the second is just if u dont wanna bother with specifying server name, but if ure in two channels with the same name itll pick one basically at random!)
+def getChannelByServerAndName(server_name : str, channel_name : str):
+    for server in bot.servers:
+        if server.name == server_name or server_name == "":
+            for channel in server.channels:
+                if not channel.type == discord.ChannelType.voice and channel.name == channel_name:
+                    return channel
+    return None
+
+def getChannelByName(channel_name : str):
+    return getChannelByServerAndName("",channel_name)
+    
+def getServerByName(server_name : str):
+    for server in bot.servers:
+        if server.name == server_name:
+            return server
+########
+
+########this gets an user by its name (it only checks our main server to avoid issues with duplicate usernames)
+def getUserByName(userName):
+    global mainserver
+    server = getServerByName(mainserver)
+    for user in server.members:
+        if user.name.lower() == userName.lower():
+            return user
+    
+    return None
+########
+
+########this gets an user by its id
+def getUserById(userId):
+    global mainserver
+    server = getServerByName(mainserver)
+    for user in server.members:
+        if user.id == userId:
+            return user
+    
+    return None
+########
+
+########custom on_message to handle hidden commands and lastmessage info
+@asyncio.coroutine
+def on_message(self, message):
+    global lastmessage
+    global lastmessagetime
+    global mainserver
+    global mainchannel
+    global isgoof
+    #update last message
+    if not message.channel.is_private:
+        if not lastmessage == message and message.channel.name == mainchannel and message.channel.server.name == mainserver:
+            lastmessage = message
+            lastmessagetime = datetime.datetime.now()
+            #always sets isgoof to false, if the message came from us, and we were goofing, it will be set to true right after anyway
+            isgoof = False
+    
+    #here you can add any commands u want hidden or whatever. use message.content and message.author.id
+    if message.content.startswith("/"):
+        commandEnd = message.content.find(" ")
+        command = ""
+        if commandEnd > -1:
+            command = message.content[1:commandEnd]
+        else:
+            command = message.content[1:]
+        #i want this to work only on pm
+        if command == "stalk" and message.channel.is_private:
+            print(message.content[len(command)+2:].strip())
+            yield from stalk(message.channel,message.content[len(command)+2:].strip().split(","))
+    
+    #do the default call to process_commands, if u wanna use super for this go ahead, idgaf
+    yield from self.process_commands(message)
+
+#replacing default on_message from commands.Bot class
+commands.Bot.on_message = on_message
+########
+
+
+########hidden stalk function! we will use this to make links between people and give them importance weights etc
+@asyncio.coroutine
+def stalk(channel,args : str):
+    if len(args) >= 2:
+        operation = args[0].strip()
+        args.remove(args[0])
+        stalkeename = args[0].strip()
+        args.remove(args[0])
+        stalkee = getUserByName(stalkeename)
+        if stalkee is not None:
+            userdbconn = sqlite3.connect('users.db')
+            userdb = userdbconn.cursor()
+            
+            ####/stalk add, -userName-
+            if operation == "add":
+                userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+                result = userdb.fetchone()
+                if(result is None):
+                    userdb.execute("INSERT INTO discordUsers VALUES(?,?)",(int(stalkee.id),5,))
+                    yield from bot.send_message(channel,'Added **@'+stalkee.name+'** to userList, his discord userID is **'+stalkee.id+'**.\r\n'+
+                'His importance weight has been set to the default **5**, please use **/stalk weight, -userName-, -weight-** to set it.\r\n'+
+                'Use **/stalk addchar, -userName-, -char-** to add Tibia characters to this user.')
+                else:
+                    yield from bot.send_message(channel,'User **@'+stalkee.name+'** is already in users db.')
+            ####
+            
+            ####/stalk remove, -userName-
+            elif operation == "remove":
+                #there should probably be either a confirmation or a backup for this but w/e
+                userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+                result = userdb.fetchone()
+                if(result is not None):
+                    userdb.execute("DELETE FROM discordUsers WHERE id = ?",(int(stalkee.id),))
+                    yield from bot.send_message(channel,'Removed **@'+stalkee.name+'** from userList.')
+                else:
+                    yield from bot.send_message(channel,'User **@'+stalkee.name+'** not found in users db.')
+            ####
+            
+            ####/stalk weight, -userName-, -newWeight-
+            elif operation == "weight":
+                userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+                result = userdb.fetchone()
+                if(result is not None):
+                    #hahaha this if is fucking cancer but oh well it works
+                    if len(args) == 0 or not args[0].isdigit() or (args[0].isdigit() and (int(args[0]) > 5 or int(args[0]) < 1)):
+                        yield from bot.send_message(channel,'Usage for **weight** is **/stalk weight, -userName-, -newWeight-**.\r\n'+
+                    'Valid weights are 1 through 5.')
+                    else:
+                        newWeight = int(args[0])
+                        userdb.execute("UPDATE discordUsers SET weight = ? WHERE id = ?",(newWeight,int(stalkee.id),))
+                        yield from bot.send_message(channel,'**@'+stalkee.name+'**\'s weight has been set to **'+str(newWeight)+'**.')
+                else:
+                    yield from bot.send_message(channel,'User **@'+stalkee.name+'** not found in users db.\r\n'+
+                'Use **/stalk add, -userName-** to add this user.')
+            ####
+            
+            ####/stalk addchar, -userName-, -char-
+            elif operation == "addchar":
+                userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+                result = userdb.fetchone()
+                if(result is not None):
+                    if len(args) == 0:
+                        yield from bot.send_message(channel,'Usage for **addchar** is **/stalk addchar, -userName-, -char-**.')
+                    else:
+                        charName = str(args[0]).title()
+                        userdb.execute("SELECT discordUser, charName FROM tibiaChars WHERE charName LIKE ?",(charName,))
+                        result = userdb.fetchone()
+                        if(result is None):
+                            userdb.execute("INSERT INTO tibiaChars VALUES(?,?)",(int(stalkee.id),charName,))
+                            yield from bot.send_message(channel,'**'+charName+'** has been added to **@'+stalkee.name+'**\'s Tibia character list.\r\n'+
+                        'Use **/stalk removechar, -userName-, -char-** to remove Tibia chars from an user.')
+                        else:
+                            charOwner = getUserById(str(result[0]))
+                            if charOwner is not None:
+                                yield from bot.send_message(channel,'Tibia character **'+charName+'** is already assigned to user **@'+charOwner.name+'**.')
+                            else:
+                                #the old char owner doesnt exist any more for some reason, so just assign it to this new user
+                                userdb.execute("UPDATE tibiaChars SET discordUser = ? WHERE charName = ?",(int(stalkee.id),charName,))
+                                yield from bot.send_message(channel,'**'+charName+'** has been added to **@'+stalkee.name+'**\'s Tibia character list.\r\n'+
+                            'Use **/stalk removechar, -userName-, -char-** to remove Tibia chars from an user.')
+                else:
+                    yield from bot.send_message(channel,'User **@'+stalkee.name+'** not found in users db.\r\n'+
+                'Use **/stalk add, -userName-** to add this user.')
+            ####
+            
+            ####/stalk removechar, -userName-, -char-
+            elif operation == "removechar":
+                userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+                result = userdb.fetchone()
+                if(result is not None):
+                    if len(args) == 0:
+                        yield from bot.send_message(channel,'Usage for **removechar** is **/stalk removechar, -userName-, -char-**.')
+                    else:
+                        charName = str(args[0]).title()
+                        userdb.execute("SELECT discordUser, charName FROM tibiaChars WHERE discordUser LIKE ? AND charName LIKE ?",(int(stalkee.id),charName,))
+                        result = userdb.fetchone()
+                        if(result is not None):
+                            userdb.execute("DELETE FROM tibiaChars WHERE discordUser LIKE ? AND charName LIKE ?",(int(stalkee.id),charName,))
+                            yield from bot.send_message(channel,'**'+charName+'** has been removed from **@'+stalkee.name+'**\'s Tibia character list.')
+                        else:
+                            yield from bot.send_message(channel,'**'+charName+'** is not in **@'+stalkee.name+'**\'s Tibia character list.')
+                else:
+                    yield from bot.send_message(channel,'User **@'+stalkee.name+'** not found in users db.\r\n'+
+                'Use **/stalk add, -userName-** to add this user.')
+            ####
+            
+            ####unknown operation
+            else:
+                yield from bot.send_message(channel,'Unknown operation for /stalk: **'+operation+'**')
+            ####
+            
+            userdbconn.commit()
+            userdb.execute("SELECT id, weight FROM discordUsers WHERE id LIKE ?",(int(stalkee.id),))
+            result = userdb.fetchone()
+            if(result is not None):
+                userinfo = dict(zip(['id','weight'],result))
+                print('id = '+str(userinfo['id']))
+                print('weight = '+str(userinfo['weight']))
+                
+            userdb.execute("SELECT discordUser, charName FROM tibiaChars WHERE discordUser LIKE ?",(int(stalkee.id),))
+            result = userdb.fetchone()
+            if(result is not None):
+                userinfo = dict(zip(['discordUser','charName'],result))
+                print('discordUser = '+str(userinfo['discordUser']))
+                print('charName = '+str(userinfo['charName']))
+            userdbconn.close()
+        else:
+            yield from bot.send_message(channel,'User **@'+stalkeename.title()+'** not found in server **'+mainserver+'**.')
+    else:
+        yield from bot.send_message(channel,'Valid arguments for /stalk are **add**, **remove**, **weight**, **addchar**, **removechar**.')
+########
+
+########handle userList
+def userList(operation,stalkee=""):
+    #stalkeesdb = sqlite3.connect('userList.db')
+    if operation == "load":
+        print("load")
+        #stalkeesdb.execute("SELECT title, userId FROM Users WHERE name LIKE ?",(name,))
+        #result = c.fetchall()
+    elif operation == "append" and stalkee != "":
+        userlist.append(stalkee)
+########
+
+@bot.command()
+@asyncio.coroutine
+def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        yield from bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    yield from bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+@asyncio.coroutine
+def choose(*choices : str):
+    """Chooses between multiple choices."""
+    yield from bot.say(random.choice(choices))
+
+
+@bot.command()
+@asyncio.coroutine
+def joined(member : discord.Member):
+    """Says when a member joined."""
+    yield from bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+bot.run(username, password)
---
+++ tibia.py
@@ -0,0 +1,240 @@
+import discord
+from discord.ext import commands
+import asyncio
+import urllib.request
+import urllib
+import re
+import math
+import random
+import sqlite3
+
+def checkGuildOnline(guildname):
+    ##Fetch webpage
+    page = urllib.request.urlopen('https://secure.tibia.com/community/?subtopic=guilds&page=view&GuildName='+urllib.parse.quote(guildname)+'&onlyshowonline=1')
+    content = page.read()
+    #Check if guild exists (in a really lazy way)
+    try:
+        content.decode().index("Information")
+    except Exception:
+        return 'NE'
+    #Trimming content string to reduce load
+    startIndex = content.decode().index("<td>Status</td>")
+    endIndex = content.decode().index("name=\"Show All\"")
+    content = content[startIndex:endIndex]
+
+    #Regex pattern to fetch information
+    regex_members = r'<TD>(.+?)</TD>\s</td><TD><A HREF="https://secure.tibia.com/community/\?subtopic=characters&name=(.+?)">.+?</A> *\(*(.*?)\)*</TD>\s<TD>(.+?)</TD>\s<TD>(.+?)</TD>\s<TD>(.+?)</TD>'
+    pattern = re.compile(regex_members,re.MULTILINE+re.S)
+
+    m = re.findall(pattern,content.decode())
+    member_list = [];
+    #Check if list is empty
+    if m:
+        #Building dictionary list from members
+        for (rank, name, title, vocation, level, joined) in m:
+            rank = '' if (rank == '&#160;') else rank
+            name = urllib.parse.unquote_plus(name)
+            joined = joined.replace('&#160;','-')
+            member_list.append({'rank' : rank, 'name' : name, 'title' : title,
+            'vocation' : vocation, 'level' : level, 'joined' : joined})
+        return member_list
+    
+    return 'NO'
+
+def checkChar(name):
+    char = {'guild' : ''}
+    #Fetch website
+    page = urllib.request.urlopen('https://secure.tibia.com/community/?subtopic=characters&name='+urllib.parse.quote(name))
+    content = page.read()
+    #Checking if character exists
+    try:
+        content.decode().index("Vocation:")
+    except Exception:
+        return
+    #Trimming content to reduce load
+    startIndex = content.decode().index("BoxContent")
+    endIndex = content.decode().index("<B>Search Character</B>")
+    content = content[startIndex:endIndex]
+
+   
+    #Name
+    m = re.search(r'Name:</td><td>([^<]+)\s',content.decode())
+    if m:
+        char['name'] = m.group(1).strip()
+
+    #Vocation
+    m = re.search(r'Vocation:</td><td>([^<]+)',content.decode())
+    if m:
+        char['vocation'] = m.group(1)
+
+    #Level
+    m = re.search(r'Level:</td><td>(\d+)',content.decode())
+    if m:
+        char['level'] = int(m.group(1))
+
+    #World
+    m = re.search(r'World:</td><td>([^<]+)',content.decode())
+    if m:
+        char['world'] = m.group(1)
+
+    #Residence (City)        
+    m = re.search(r'Residence:</td><td>([^<]+)',content.decode())
+    if m:
+        char['residence'] = m.group(1)
+
+    #Sex, only stores pronoun
+    m = re.search(r'Sex:</td><td>([^<]+)',content.decode())
+    if m:
+        if m.group(1) == 'male':
+            char['pronoun'] = 'He'
+        else:
+            char['pronoun'] = 'She'
+            
+    #Guild rank
+    m = re.search(r'membership:</td><td>([^<]+)\sof the',content.decode())
+    if m:
+        char['rank'] = m.group(1)
+        #Guild membership
+        m = re.search(r'GuildName=.*?([^"]+).+',content.decode())
+        if m:
+            char['guild'] = urllib.parse.unquote_plus(m.group(1))
+        
+    return char
+
+def getItem(name):
+    #Reading item database
+    c = sqlite3.connect('Database.db').cursor()
+    #Search query
+    c.execute("SELECT title, vendor_value FROM Items WHERE name LIKE ?",(name,))
+    result = c.fetchone()
+    #Checking if item exists
+    if(result is not None):
+        #Turning result tuple into dictionary
+        item = dict(zip(['name','value'],result))
+        #If value is greater than 0, we search npcs that buy it
+        if(item['value'] and item['value'] > 0):
+            c.execute("SELECT Items.title, value, NPCs.title, city"+
+            " FROM Items, SellItems, NPCs WHERE Items.name LIKE ?"+
+            " AND SellItems.itemid = Items.id AND NPCs.id = vendorid"+
+            " ORDER BY value DESC LIMIT 1",(name,))
+            result = c.fetchone()
+            #This shouldn't be neccesary...
+            #Note: Items that are not sellable to NPCs take the value from the buyprice
+            #So if the item's value is greater than 0, it might still have no npc key
+            if(result is not None):
+                item = dict(zip(['name','value','npc','city'],result))
+        #Only used for the cases where the value doesn't exist, fuck that noise
+        if(not item['value']):
+            item['value'] = 0
+        return item
+    return
+
+class Tibia():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(pass_context=True)
+    @asyncio.coroutine
+    def check(self,ctx,*name : str):
+        """Tells you information about a character"""
+        name = " ".join(name)
+        char = checkChar(name)
+        if char:
+            yield from self.bot.say('**'+char['name']+'** is a level '+str(char['level'])+
+            ' __'+char['vocation']+'__. '+char['pronoun']+' resides in __'+
+            char['residence']+'__, in the world __'+ char['world']+'__.\n'+
+            ((char['pronoun']+' is a '+char['rank']+' of the **'+char['guild']+'**.') if (char['guild']) else ''))
+        else:
+            yield from self.bot.say('That character doesn\'t exist.')
+
+    @commands.group(pass_context=True)
+    @asyncio.coroutine
+    def share(self,ctx,*param : str):
+        """Shows the sharing range for that level or character"""
+        level = 0
+        name = ''
+        #Check if param is numeric
+        try:
+            level = int(param[0])
+        #If it's not numeric, then it must be a char's name
+        except ValueError:
+            name = " ".join(param)
+            char = checkChar(name)
+            if char:
+                level = int(char['level']);
+                name = char['name'];
+            else:
+                yield from self.bot.say('There is no character with that name.');
+                return
+        if(level <= 0):
+            replies = ["Invalid level.", "I don't think that's a valid level.",
+            "You're doing it wrong!", "Nope, you can't share with anyone.",
+            "You probably need a couple more levels"]
+            yield from self.bot.say(random.choice(replies))
+            return
+        low = int(math.ceil(level*2.0/3.0))
+        high = int(math.floor(level*3.0/2.0))
+        if(name == ''):
+            yield from self.bot.say('A level '+str(level)+' can share experience with levels **'+str(low)+
+        '** to **'+str(high)+'**.')
+        else:
+            yield from self.bot.say('**'+name+'** ('+str(level)+') can share experience with levels **'+str(low)+
+        '** to **'+str(high)+'**.')
+            
+        
+
+    @commands.group(pass_context=True)
+    @asyncio.coroutine
+    def guild(self,ctx,*guildname : str):
+        """Who is online in a guild"""
+        guildname = " ".join(guildname).title()
+        onlinelist = checkGuildOnline(guildname)
+        if onlinelist == 'NE':
+            yield from self.bot.say('The guild '+urllib.parse.unquote_plus(guildname)+' doesn\'t exist.')
+        elif onlinelist == 'NO':
+            yield from self.bot.say('Nobody is online on '+urllib.parse.unquote_plus(guildname)+'.')
+        else:
+            result = ('There '+
+            ('are' if (len(onlinelist) > 1) else 'is')+' '+str(len(onlinelist))+' player'+
+            ('s' if (len(onlinelist) > 1) else '')+' online in **'+guildname+'**:')
+            for member in onlinelist:
+                result += '\n'  
+                if(member['rank'] != ''):
+                    result += '__'+member['rank']+'__\n'
+                result += '\t'+member['name']
+                result += (' (*'+member['title']+'*)' if (member['title'] != '') else '')
+                result += ' -- '+member['level']+' '
+                vocAbb = {'None' : 'N', 'Druid' : 'D', 'Sorcerer' : 'S', 'Paladin' : 'P', 'Knight' : 'K',
+                'Elder Druid' : 'ED', 'Master Sorcerer' : 'MS', 'Royal Paladin' : 'RP', 'Elite Knight' : 'EK'}
+                try:
+                    result += vocAbb[member['vocation']]
+                except KeyError:
+                    result += 'N'            
+                
+            yield from self.bot.say(result)
+            
+    @commands.group(pass_context=True)
+    @asyncio.coroutine
+    def itemprice(self,ctx,*itemname : str):
+        """Checks an item's NPC price"""
+        itemname = " ".join(itemname).strip()
+        item = getItem(itemname)
+        if(item is not None):   
+            #Check if item is sellable to NPC
+            if('npc' in item):
+                city = item['city'].title()
+                #If it's a djinn, show the race instead of city
+                if(item['npc'] == 'Alesar' or item['npc'] == 'Yaman'):
+                    city = 'Green Djinn'
+                elif(item['npc'] == 'Nah\'Bob' or item['npc'] == 'Haroun'):
+                    city = 'Blue Djinn'
+                #TODO: Replace Yasir's "varies" with today's city
+                yield from self.bot.say('**'+item['name']+'** can be sold to **'
+                +item['npc']+'** ('+city+') for **'+('{0:,}'.format(item['value']))+'** gold coins.')
+            else:
+                yield from self.bot.say('**'+item['name']+'** can\'t be sold to NPCs.')
+        else:
+            yield from self.bot.say("I couldn't find an item with that name.")
+
+def setup(bot):
+    bot.add_cog(Tibia(bot))
