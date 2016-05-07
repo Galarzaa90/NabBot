25362ed6ce30d0cde5e6656ae9c8e051d8573d87
Allan Galarza <allan.galarza@gmail.com>
2016-05-07 10:02:25 -0700

    Fixing a variable name changed in last commit

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ deathmessages_monster = [[100,"RIP **{0}** ({2}), you died the way you lived- in
 [100,"{4}**{3}** killed **{0}** at level {2}. Shame "+str(chr(0x0001f514))+" shame "+str(chr(0x0001f514))+" shame "+str(chr(0x0001f514))],
 [50,"**{0}** ({2}) is no more! /{5}/ has ceased to be! /{5}/'s expired and gone to meet {6} maker! /{5}/'s a stiff! Bereft of life, {5} rests in peace! If {5} hadn't respawned {5}'d be pushing up the daisies! /{6}/ metabolic processes are now history! /{5}/'s off the server! /{5}/'s kicked the bucket, {5}'s shuffled off {6} mortal coil, kissed {4}**{3}**'s butt, run down the curtain and joined the bleeding choir invisible!! THIS IS AN EX-**\{0}/**."],
 [100,"RIP **{0}** ({2}), we hardly knew you! (^That ^**{3}** got to know you pretty well though "+str(chr(0x0001f609))+")"],
-[80,"A priest, {4}**{3}** and **{0}** ({2}) walk into a bar. "+str(chr(0x0001f480))+"ONLY ONE GETS OUT."+str(chr(0x0001f480))],
+[80,"A priest, {4}**{3}** and **{0}** ({2}) walk into a bar. "+str(chr(0x0001f480))+"ONLY ONE WALKS OUT."+str(chr(0x0001f480))],
 [90,"RIP **{0}** ({2}), you were strong. ^The ^**{3}** was stronger."]]
 ##deaths by player
 deathmessages_player = [[100,"**{0}** ({2}) got rekt! **{3}** ish pekay!"],
@@ -78,5 +78,5 @@ deathmessages_player = [[100,"**{0}** ({2}) got rekt! **{3}** ish pekay!"],
 
 ### Channels to look for users ###
 ## I don't want to change the other variable cause I don't want goof messages on the main channel yet
-mainserver = "Redd Alliance/Bald Dwarfs"
+search_server = "Redd Alliance/Bald Dwarfs"
 search_channel = "general-chat"
