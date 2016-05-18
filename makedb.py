64f122c23202a8f019df4e054151ed98679e0176
Allan Galarza <allan.galarza@gmail.com>
2016-05-18 07:35:46 -0700

    Updated makedb.py to reflect the database changes made in commit 9da81219902c4b895fbb65729c2bc84a8d10fea2

--- makedb.py
+++ makedb.py
@@ -7,12 +7,28 @@ c = conn.cursor()
 
 #If this is run with 'force' as a parameter, it drops all the tables and then creates them again
 if('force' in sys.argv):
-    c.execute("DROP TABLE IF EXISTS discordUsers")
-    c.execute("DROP TABLE IF EXISTS tibiaChars")
+    c.execute("DROP TABLE IF EXISTS discord_users")
+    c.execute("DROP TABLE IF EXISTS chars")
+    c.execute("DROP TABLE IF EXISTS char_levelups")
     
 #Create the table if they don't exist already
-c.execute("CREATE TABLE IF NOT EXISTS discordUsers(id INT, weight INT)")
-c.execute("CREATE TABLE IF NOT EXISTS tibiaChars(discordUser INT, charName TEXT, lastLevel INT, lastDeathTime TEXT)")
+c.execute("""CREATE TABLE discord_users (
+        id	INTEGER NOT NULL,
+        weight	INTEGER DEFAULT 5,
+        PRIMARY KEY(id)
+        )""")
+c.execute("""CREATE TABLE chars (
+        id	INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id	INTEGER,
+        name	TEXT,
+        last_level	INTEGER DEFAULT -1,
+        last_death_time	TEXT
+        )""")
+c.execute("""CREATE TABLE char_levelups (
+        char_id	INTEGER,
+        level	INTEGER,
+        date	INTEGER
+        )""")
     
 conn.commit()
 conn.close()
