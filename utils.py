Galarzaa90/NabBot

commit d7fc798e5d675910e65a24196f8b14e5249c299f

Changes in database schema - Renamed table "discord_users" to "users" - Created table "user_servers" - Added column "world" to table "char"

utils.py
@@ -52,7 +52,7 @@ log.addHandler(consoleHandler)
 userDatabase = sqlite3.connect(USERDB)
 tibiaDatabase = sqlite3.connect(TIBIADB)
 
-DB_LASTVERSION = 5
+DB_LASTVERSION = 6
 
 # Tibia.com URLs:
 url_character = "https://secure.tibia.com/community/?subtopic=characters&name="
@@ -64,7 +64,6 @@ def initDatabase():
 
     # Database file is automatically created with connect, now we have to check if it has tables
     print("Checking database version...")
-    db_version = 0
     try:
         c = userDatabase.cursor()
         c.execute("SELECT COUNT(*) as count FROM sqlite_master WHERE type = 'table'")
@@ -91,7 +90,7 @@ def initDatabase():
         c.execute("SELECT tbl_name FROM sqlite_master WHERE type = 'table' AND name LIKE 'db_info'")
         result = c.fetchone()
         # If there's no version value, version 1 is assumed
-        if (result is None):
+        if result is None:
             c.execute("""CREATE TABLE db_info (
                       key TEXT,
                       value TEXT
@@ -139,6 +138,16 @@ def initDatabase():
         if db_version == 4:
             # Added 'name' column to 'discord_users' table to save their names for external use
             c.execute("ALTER TABLE discord_users ADD name TEXT")
+            db_version += 1
+        if db_version == 5:
+            # Added 'world' column to 'chars', renamed 'discord_users' to 'users', created table 'user_servers'
+            c.execute("ALTER TABLE chars ADD world TEXT")
+            c.execute("ALTER TABLE discord_users RENAME TO users")
+            c.execute("""CREATE TABLE user_servers (
+                      id INTEGER,
+                      server INTEGER,
+                      PRIMARY KEY(id)
+                      );""")
             db_version += 1
         print("Updated database to version {0}".format(db_version))
         c.execute("UPDATE db_info SET value = ? WHERE key LIKE 'version'", (db_version,))

mod.py
@@ -40,11 +40,11 @@ class Mod:
                 if user is None:
                     yield from self.bot.say("I don't see any user named **{0}**.".format(params[0]))
                     return
-                c.execute("SELECT id from discord_users WHERE id LIKE ?", (user.id,))
+                c.execute("SELECT id from users WHERE id LIKE ?", (user.id,))
                 if c.fetchone() is not None:
                     yield from self.bot.say("**@{0}** is already registered.".format(user.display_name))
                     return
-                c.execute("INSERT INTO discord_users(id,name) VALUES (?,?)", (user.id, user.display_name,))
+                c.execute("INSERT INTO users(id,name) VALUES (?,?)", (user.id, user.display_name,))
                 yield from self.bot.say("**@{0}** was registered successfully.".format(user.display_name))
 
             # Add char & Add account common operations
@@ -94,10 +94,10 @@ class Mod:
                     c.execute("INSERT INTO chars (name,last_level,vocation,user_id) VALUES (?,?,?,?)",
                               (char['name'], char['level'] * -1, char['vocation'], user.id))
                     # Check if user is already registered
-                    c.execute("SELECT id from discord_users WHERE id = ?", (user.id,))
+                    c.execute("SELECT id from users WHERE id = ?", (user.id,))
                     result = c.fetchone()
                     if result is None:
-                        c.execute("INSERT INTO discord_users(id,name) VALUES (?,?)", (user.id, user.display_name,))
+                        c.execute("INSERT INTO users(id,name) VALUES (?,?)", (user.id, user.display_name,))
                         yield from self.bot.say("**@{0}** was registered successfully.".format(user.display_name))
                     yield from self.bot.say("**{0}** was registered successfully to this user.".format(char['name']))
                     return
@@ -141,10 +141,10 @@ class Mod:
                             (char['name'], char['level'] * -1, char['vocation'], user.id)
                         )
                         yield from self.bot.say("**{0}** was registered successfully to this user.".format(char['name']))
-                    c.execute("SELECT id from discord_users WHERE id = ?", (user.id,))
+                    c.execute("SELECT id from users WHERE id = ?", (user.id,))
                     result = c.fetchone()
                     if result is None:
-                        c.execute("INSERT INTO discord_users(id,name) VALUES (?,?)", (user.id, user.display_name,))
+                        c.execute("INSERT INTO users(id,name) VALUES (?,?)", (user.id, user.display_name,))
                         yield from self.bot.say("**@{0}** was registered successfully.".format(user.display_name))
                     return
 
@@ -174,7 +174,7 @@ class Mod:
                 # Searching users in server
                 user = getUserByName(params[0], search_pm=False)
                 # Searching users in database
-                c.execute("SELECT id, name from discord_users WHERE name LIKE ?", (params[0],))
+                c.execute("SELECT id, name from users WHERE name LIKE ?", (params[0],))
                 result = c.fetchone()
                 # Users in database and not in servers
                 if result is not None and getUserById(result['id'], search_pm=False) is None:
@@ -194,7 +194,7 @@ class Mod:
                     yield from self.bot.say("I don't see any user named **{0}**.".format(params[0]))
                     return
 
-                c.execute("DELETE FROM discord_users WHERE id = ?", (delete_id,))
+                c.execute("DELETE FROM users WHERE id = ?", (delete_id,))
                 c.execute("SELECT name FROM chars WHERE user_id = ?", (delete_id,))
                 result = c.fetchall()
                 if len(result) >= 1:
@@ -216,7 +216,7 @@ class Mod:
                 return
             # Purge
             if subcommand == "purge":
-                c.execute("SELECT id FROM discord_users")
+                c.execute("SELECT id FROM users")
                 result = c.fetchall()
                 if result is None:
                     yield from self.bot.say("There are no users registered.")
@@ -229,18 +229,18 @@ class Mod:
                     if user is None:
                         delete_users.append((row["id"],))
                 if len(delete_users) > 0:
-                    c.executemany("DELETE FROM discord_users WHERE id = ?", delete_users)
+                    c.executemany("DELETE FROM users WHERE id = ?", delete_users)
                     yield from self.bot.say("{0} user(s) no longer in the server were removed.".format(c.rowcount))
 
                 # Deleting chars with non-existent user
-                c.execute("SELECT name FROM chars WHERE user_id NOT IN (SELECT id FROM discord_users)")
+                c.execute("SELECT name FROM chars WHERE user_id NOT IN (SELECT id FROM users)")
                 result = c.fetchall()
                 if len(result) >= 1:
                     chars = ["**" + i["name"] + "**" for i in result]
                     reply = "{0} char(s) were assigned to a non-existent user and were deleted:\n\t".format(len(result))
                     reply += "\n\t".join(chars)
                     yield from self.bot.say(reply)
-                    c.execute("DELETE FROM chars WHERE user_id NOT IN (SELECT id FROM discord_users)")
+                    c.execute("DELETE FROM chars WHERE user_id NOT IN (SELECT id FROM users)")
 
                 # Removing deleted chars
                 c.execute("SELECT name,last_level,vocation FROM chars")
@@ -275,10 +275,10 @@ class Mod:
                     yield from self.bot.say("{0} char(s) were renamed.".format(c.rowcount))
 
                 # Remove users with no chars
-                c.execute("SELECT id FROM discord_users WHERE id NOT IN (SELECT user_id FROM chars)")
+                c.execute("SELECT id FROM users WHERE id NOT IN (SELECT user_id FROM chars)")
                 result = c.fetchall()
                 if len(result) >= 1:
-                    c.execute("DELETE FROM discord_users WHERE id NOT IN (SELECT user_id FROM chars)")
+                    c.execute("DELETE FROM users WHERE id NOT IN (SELECT user_id FROM chars)")
                     yield from self.bot.say("{0} user(s) with no characters were removed.".format(c.rowcount))
 
                 # Remove level ups of removed characters
@@ -317,7 +317,7 @@ class Mod:
                         "\n\t".join(empty_members)))
             # Checknames
             if subcommand == "refreshnames":
-                c.execute("SELECT id FROM discord_users")
+                c.execute("SELECT id FROM users")
                 result = c.fetchall()
                 if len(result) <= 0:
                     yield from self.bot.say("There are no registered users.")
@@ -326,7 +326,7 @@ class Mod:
                 for user in result:
                     update_users.append(
                         ("unknown" if getUserById(user[0]) is None else getUserById(user[0]).display_name, user["id"]))
-                c.executemany("UPDATE discord_users SET name = ? WHERE id LIKE ?", update_users)
+                c.executemany("UPDATE users SET name = ? WHERE id LIKE ?", update_users)
                 yield from self.bot.say("Usernames updated successfully.")
         finally:
             c.close()

nabbot.py
@@ -550,7 +550,7 @@ def im(ctx, *charname: str):
             yield from bot.say(notallowed_message)
             return
         # Check that this user doesn't exist or has no chars added to it yet.
-        c.execute("SELECT id from discord_users WHERE id = ?", (user.id,))
+        c.execute("SELECT id from users WHERE id = ?", (user.id,))
         result = c.fetchone()
         if result is not None:
             c.execute("SELECT name,user_id FROM chars WHERE user_id LIKE ?", (user.id,))
@@ -560,7 +560,8 @@ def im(ctx, *charname: str):
                 return
         else:
             # Add the user if it doesn't exist
-            c.execute("INSERT INTO discord_users(id,name) VALUES (?,?)", (user.id, user.display_name,))
+            c.execute("INSERT INTO users(id,name) VALUES (?,?)", (user.id, user.display_name,))
+            c.execute("INSERT INTO user_servers(id,server) VALUES (?,?)", (user.id, ctx.message.server.id,))
 
         char = yield from getPlayer(charname)
         if type(char) is not dict:
@@ -601,8 +602,8 @@ def im(ctx, *charname: str):
             log.info("Character {0} was reasigned to {1.display_name} (ID: {1.id}) from /im. (Previous owner (ID: {2}) was not found)".format(char['name'], user, char['prevowner']))
         for char in added:
             c.execute(
-                "INSERT INTO chars (name,last_level,vocation,user_id) VALUES (?,?,?,?)",
-                (char['name'], char['level']*-1, char['vocation'], user.id)
+                "INSERT INTO chars (name,last_level,vocation,user_id, world) VALUES (?,?,?,?,?)",
+                (char['name'], char['level']*-1, char['vocation'], user.id, char["world"])
             )
             log.info("Character {0} was asigned to {1.display_name} (ID: {1.id}) from /im.".format(char['name'], user))
 
@@ -627,7 +628,7 @@ def online():
     try:
         for char in globalOnlineList:
             char = char.split("_", 1)[1]
-            c.execute("SELECT name, user_id, vocation, last_level FROM chars WHERE name LIKE ?",(char,))
+            c.execute("SELECT name, user_id, vocation, last_level FROM chars WHERE name LIKE ?", (char,))
             result = c.fetchone()
             if result:
                 # This will always be true unless a char is removed from chars in between globalOnlineList updates
@@ -830,6 +831,7 @@ def events(ctx, *args: str):
     finally:
         userDatabase.commit()
         c.close()
+
 
 @bot.command(pass_context=True, no_pm=True, name="server", aliases=["serverinfo", "server_info"])
 @asyncio.coroutine

commit 310d1353bc7cd6a78dc807dac13e52f8f3f82fac

Fixed bug in /about, introduced in d7fc798e5d675910e65a24196f8b14e5249c299f

utils.py
@@ -438,7 +438,7 @@ def getAboutContent() -> discord.Embed:
     if not lite_mode:
         c = userDatabase.cursor()
         try:
-            c.execute("SELECT COUNT(*) as count FROM discord_users")
+            c.execute("SELECT COUNT(*) as count FROM users")
             result = c.fetchone()
             if result is not None:
                 user_count = result["count"]