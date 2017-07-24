92246adaebc4aacf2a95d4a1f49e785e083397df
Nezune <kinight@gmail.com>
2017-07-24 16:37:25 -0300

    added item_new debug function for loot.db

--- utils/loot.py
+++ utils/loot.py
@@ -375,7 +375,7 @@ async def item_show(item):
               (item,))
     item_list = c.fetchall()
     if len(item_list) == 0:
-        return None
+        return None,None
     output_image = Image.new("RGBA", (33*len(item_list)-1, 32), (255, 255, 255, 255))
     x = 0
     for i in item_list:
@@ -386,7 +386,7 @@ async def item_show(item):
     img_byte_arr = io.BytesIO()
     output_image.save(img_byte_arr, format='png')
     img_byte_arr = img_byte_arr.getvalue()
-    return img_byte_arr
+    return img_byte_arr,item_list[0]
 
 
 async def item_add(item,frame):
@@ -424,6 +424,44 @@ async def item_add(item,frame):
     img_byte_arr = img_byte_arr.getvalue()
     return img_byte_arr
 
+async def item_new(item,frame,group,value):
+    if item is None or group is None:
+        print("here")
+        return None
+        
+    c = lootDatabase.cursor()
+    c.execute("SELECT * FROM Items "
+              "WHERE name LIKE ?",
+              (item,))
+    item_list = c.fetchall()
+    if not len(item_list) == 0:
+        print("no, here")
+        return None
+
+    frame_crop = crop_item(frame)
+    frame_color = get_item_color(frame)
+    frame_size = get_item_size(frame_crop)
+    frame_ByteArr = io.BytesIO()
+    frame.save(frame_ByteArr, format='PNG')
+    frame_ByteArr = frame_ByteArr.getvalue() 
+    frameStr = pickle.dumps(frame_ByteArr)
+    c.execute("INSERT INTO Items(name,`group`,priority,value,frame,sizeX,sizeY,size,red,green,blue) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(item,group,0,value,frameStr,frame_crop.size[0],frame_crop.size[1],frame_size,frame_color[0],frame_color[1],frame_color[2]))
+    
+    c.execute("SELECT * FROM Items "
+              "WHERE name LIKE ?",
+              (item,))
+    item_list = c.fetchall()
+    output_image = Image.new("RGBA", (33*len(item_list)-1, 32), (255, 255, 255, 255))
+    x = 0
+    for i in item_list:
+        i_image = pickle.loads(i['frame'])
+        i_image = Image.open(io.BytesIO(bytearray(i_image)))
+        output_image.paste(i_image, (x*33, 0))
+        x +=1
+    img_byte_arr = io.BytesIO()
+    output_image.save(img_byte_arr, format='png')
+    img_byte_arr = img_byte_arr.getvalue()
+    return img_byte_arr
 
 async def loot_scan(loot_image, image_name, progress_msg, progress_bar):
     debug_output = image_name
