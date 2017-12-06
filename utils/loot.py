29145c24d35fcb65f50d9dce316a4943c5f287a7
Nezune <kinight@gmail.com>
2017-12-06 16:07:26 -0300

    Fixed number offsets for 11.5

--- utils/loot.py
+++ utils/loot.py
@@ -115,10 +115,10 @@ def crop_item(item_image):
 
 
 def numberScan(itemImage):
-    number1 = itemImage.crop((7, 21, 7+8, 21+10))
-    number2 = itemImage.crop((15, 21, 15+8, 21+10))
-    number3 = itemImage.crop((23, 21, 23+8, 21+10))
-    itemNumbersImage = itemImage.crop((7, 21, 7+8*3, 21+10))
+    number1 = itemImage.crop((8, 21, 8+8, 21+10))
+    number2 = itemImage.crop((16, 21, 16+8, 21+10))
+    number3 = itemImage.crop((24, 21, 24+8, 21+10))
+    itemNumbersImage = itemImage.crop((8, 21, 8+8*3, 21+10))
     item_numbers = [number1, number2, number3]
     number_string = ""
     numbers_image = Image.new("RGBA", (24, 10), (255, 255, 255, 0))
@@ -152,7 +152,7 @@ def numberScan(itemImage):
     while py < numbers_image.size[1]:
         numbers_image_pixel = numbers_image.getpixel((px, py))
         if not is_transparent(numbers_image_pixel):
-            itemImage.putpixel((px+7, py+21), (255, 255, 0, 0))
+            itemImage.putpixel((px+8, py+21), (255, 255, 0, 0))
         px += 1
         if px == numbers_image.size[0]:
             py += 1
