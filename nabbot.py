6c84cefa71d9ca76f9be5b0da907a4c4fab4655b
Allan Galarza <allan.galarza@gmail.com>
2018-05-23 16:57:20 -0700

    Preparing for 1.1.0 release

--- nabbot.py
+++ nabbot.py
@@ -36,7 +36,7 @@ class NabBot(commands.Bot):
         # A list version is created from the dictionary
         self.tracked_worlds = {}
         self.tracked_worlds_list = []
-        self.__version__ = "1.1.0a"
+        self.__version__ = "1.1.0"
 
     async def on_ready(self):
         """Called when the bot is ready."""
