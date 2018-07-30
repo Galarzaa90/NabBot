e72d732453f22bb79b4646276f0416b4017ce876
Nezune <kinight@gmail.com>
2018-07-30 11:52:06 +0000

    get_character now uses online list data for level and vocation
    should fix the issue with commands (/share, /stats, etc.) using an outdated level for players that leveled up and are still online

--- utils/tibia.py
+++ utils/tibia.py
@@ -17,6 +17,7 @@ from discord.ext import commands
 
 from utils.config import config
 from utils.database import userDatabase, tibiaDatabase
+from utils.general import global_online_list
 from .general import log
 
 # Constants
@@ -418,6 +419,17 @@ async def get_character(name, tries=5, *, bot: commands.Bot=None) -> Optional[Ch
             result = c.fetchone()
             if result:
                 character.house["houseid"] = result["id"]
+
+    # If the character exists in the online list use data from there where possible
+    try:
+        character.level = [x.level for x in global_online_list if x.name == character.name][0]
+    except IndexError:
+        pass
+
+    try:
+        character.vocation = [x.vocation for x in global_online_list if x.name == character.name][0]
+    except IndexError:
+        pass
 
     # Database operations
     c = userDatabase.cursor()
