8d2e03f9dc80c34492e88529de6d223bdafc3467
Allan Galarza <allan.galarza@gmail.com>
2018-11-22 23:08:57 -0700

    Added magic level calculator

--- cogs/calculators.py
+++ cogs/calculators.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import logging
+import math
 
 import discord
 from discord.ext import commands
@@ -17,11 +18,65 @@ MELEE_FACTOR = {
     "sorcerer": 2
 }
 
+MAGIC_FACTOR = {
+    "knight": 3,
+    "paladin": 1.4,
+    "druid": 1.1,
+    "sorcerer": 1.1
+}
+
+MANA_PER_SEC = {
+    "knight": 1/3,
+    "paladin": 2/3,
+    "druid": 1,
+    "sorcerer": 1
+}
+
+MANA_PER_CHARGE = 600
+EXERCISE_WEAPON_GP = 262500
+EXERCISE_WEAPON_COIN = 25
+
 
 class Calculators:
     """Commands related to role management."""
     def __init__(self, bot: NabBot):
         self.bot = bot
+
+    @commands.command(usage="<current>")
+    async def magiccalc(self, ctx: NabCtx, current: int, percentage: int, target: int, *, vocation: str):
+        """Calculates the training time required to reach a target skill level.
+
+        This only applies to axe, club and sword fighting."""
+        if current >= target:
+            return await ctx.error("Target level must be greater than current skill.")
+        if 0 > percentage >= 100:
+            return await ctx.error("Percentage must be between 0 and 99.")
+        voc = self.parse_vocation(vocation)
+        if not voc:
+            return await ctx.error("Unknown vocation.")
+        mana = 0
+        factor = MAGIC_FACTOR[voc]
+        if target - current > 2:
+            mana = sum(self.magic_formula(factor, s) for s in range(current + 1, target))
+        mana += int(self.magic_formula(factor, target) * (1 - percentage / 100))
+        weapons = int(math.ceil(mana/MANA_PER_CHARGE/500))
+
+        embed = discord.Embed(title="🔢 Magic Level Calculator", colour=discord.Colour.teal())
+        embed.set_footer(text=f"From level {current} ({percentage}%) to {target} as a {voc}")
+        embed.add_field(name="Exercise Dummies",
+                        value=f"You need **{weapons:,}** exercise weapons.\n"
+                        f"Costing **{EXERCISE_WEAPON_GP*weapons:,}** gold coins "
+                        f"or **{EXERCISE_WEAPON_COIN*weapons:,}** tibia coins.\n"
+                        f"Using them would take {dt.timedelta(seconds=weapons*500*2)}.")
+        weapons = int(math.ceil(mana / (MANA_PER_CHARGE * 1.1) / 500))
+        embed.add_field(name="Expert Exercise Dummies",
+                        value=f"You need **{weapons:,}** exercise weapons.\n"
+                        f"Costing **{EXERCISE_WEAPON_GP * weapons:,}** gold coins "
+                        f"or **{EXERCISE_WEAPON_COIN * weapons:,}** tibia coins.\n"
+                        f"Using them would take {dt.timedelta(seconds=weapons * 500 * 2)}.")
+
+        embed.description = f"You need to spend **{mana:,}** mana to reach magic level {target}"
+        await ctx.send(embed=embed)
 
     @commands.command(usage="<current>")
     async def meeleecalc(self, ctx: NabCtx, current: int, percentage: int, target: int, *, vocation: str):
@@ -51,6 +106,17 @@ class Calculators:
     @classmethod
     def melee_formula(cls, factor, skill):
         return int((50 * factor**(skill-10)))
+
+    @classmethod
+    def magic_formula(cls, factor, skill):
+        return int((1600 * factor ** (skill - 10)))
+
+    @classmethod
+    def get_mana_spent(cls, current, percentage, target, factor):
+        if target - current > 2:
+            mana = sum(cls.magic_formula(factor, s) for s in range(current + 1, target))
+        mana += int(cls.magic_formula(factor, target) * (1 - percentage / 100))
+        return mana
 
     @classmethod
     def parse_vocation(cls, name):
